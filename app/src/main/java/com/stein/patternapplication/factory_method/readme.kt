427790f7327c3ae0f45ca489ad1087067b925ab7
Pavlo Kovalenko <pav.kovalenko@gmail.com>
2018-11-06 21:06:12 +0200

    Factory method patter

---
+++ app/src/main/java/com/stein/patternapplication/factory_method/readme.kt
@@ -0,0 +1,5 @@
+package com.stein.patternapplication.factory_method
+
+// use this approach for creating newInstance of Fragments can put params there
+
+//as example can be new fragment with other patterns todo continue
